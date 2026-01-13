a4214d3f9f24cd68da0a14aa6e0188c66c49afa3
daviebielcav-lab <daviebielcav@gmail.com>
2026-01-13 12:35:07 -0300

    Delete Desktop directory

--- Desktop/src/components/forms/subtypes/index.ts
+++
@@ -1,4 +0,0 @@
-export { ErroIdentificacaoForm } from "./ErroIdentificacaoForm";
-export { ExtravasamentoForm } from "./ExtravasamentoForm";
-export { RevisaoExameForm } from "./RevisaoExameForm";
-export { QuedasTraumasForm } from "./QuedasTraumasForm";
